8d476a1f4a91b208f953870e43e10e026f2c924f
seeks-imac <seeksdream@qq.com>
2023-03-19 21:49:27 +0800

    update by relation-graph-site dist

---
+++ assets/index-f47f9f88.js
@@ -0,0 +1,1 @@
+import{n as o}from"./index-d446e728.js";const s={created(){const{params:e,query:t}=this.$route,{path:r}=e;this.$router.replace({path:"/"+r,query:t})},render:function(e){return e()}};let _,u;const n={};var c=o(s,_,u,!1,l,null,null,null);function l(e){for(let t in n)this[t]=n[t]}const i=function(){return c.exports}();export{i as default};
